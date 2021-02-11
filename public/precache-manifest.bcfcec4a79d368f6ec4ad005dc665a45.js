8e997d792f42ba6bec646bd354efb517544677af
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2021-02-11 00:01:14 -0300

    v1 CalendarApp

---
+++ public/precache-manifest.bcfcec4a79d368f6ec4ad005dc665a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851eeaa7299c580631927a2776c0f866",
+    "url": "/09-calendar-app/index.html"
+  },
+  {
+    "revision": "fee3ca85886e4777a5bc",
+    "url": "/09-calendar-app/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "08493565fad5e6b3745e",
+    "url": "/09-calendar-app/static/css/main.28e07b59.chunk.css"
+  },
+  {
+    "revision": "fee3ca85886e4777a5bc",
+    "url": "/09-calendar-app/static/js/2.1ca6335a.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/09-calendar-app/static/js/2.1ca6335a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08493565fad5e6b3745e",
+    "url": "/09-calendar-app/static/js/main.5820a164.chunk.js"
+  },
+  {
+    "revision": "080e55b310b98708f54c",
+    "url": "/09-calendar-app/static/js/runtime-main.33c64f7f.js"
+  }
+]);
